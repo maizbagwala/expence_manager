87f954cd7deb25153dbfe5bc6e8ed3928dbda9a3
Kabir9611 <110175616+Kabir9611@users.noreply.github.com>
2022-07-30 00:10:34 +0530

    Splash screen, color and progressbar done

--- app/src/main/java/com/maizbagwala/expencemanager/SplashActivity.kt
+++ app/src/main/java/com/maizbagwala/expencemanager/SplashActivity.kt
@@ -1,11 +1,26 @@
 package com.maizbagwala.expencemanager
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        val bar: ProgressBar=findViewById(R.id.pbar)
+        bar.visibility= View.VISIBLE
+        val spl=Handler(Looper.getMainLooper())
+        spl.postDelayed({
+            val splash= Intent(this,MainActivity::class.java)
+            startActivity(splash)
+            finish()
+        },2000)
     }
 }
