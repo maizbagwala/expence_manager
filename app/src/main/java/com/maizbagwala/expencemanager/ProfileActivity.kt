7dd2beb5f960ef316fedae2aa2f6758eab91589c
Kabir9611 <110175616+Kabir9611@users.noreply.github.com>
2022-08-31 16:50:22 +0530

    login done

--- app/src/main/java/com/maizbagwala/expencemanager/ProfileActivity.kt
+++ app/src/main/java/com/maizbagwala/expencemanager/ProfileActivity.kt
@@ -1,6 +1,8 @@
 package com.maizbagwala.expencemanager
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
@@ -9,6 +11,9 @@ import com.google.firebase.auth.FirebaseAuth
 
 class ProfileActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+    private val sharedPref: SharedPreferences by lazy {
+        getSharedPreferences("Emspref", Context.MODE_PRIVATE)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
@@ -19,7 +24,8 @@ class ProfileActivity : AppCompatActivity() {
         findViewById<TextView>(R.id.emailid).text = email
         findViewById<TextView>(R.id.username).text = dispname
         findViewById<Button>(R.id.Signoutbtn).setOnClickListener{
-        auth.signOut()
+            auth.signOut()
+            sharedPref.edit().clear().apply()
             val ptol=Intent(this,Login::class.java)
             startActivity(ptol)
         }
